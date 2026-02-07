63c01f94ae9fe15c2e73c2f327bbd6eb34ab6daa
WernerVal-8485 <werner.valentin@imqs.co.za>
2026-02-07 20:49:31 +0200

    Added Sphinx docstrings to reviews/migrations/0002_review_is_verified_purchase.py

--- ecommerce_app/reviews/migrations/0002_review_is_verified_purchase.py
+++ ecommerce_app/reviews/migrations/0002_review_is_verified_purchase.py
@@ -4,6 +4,7 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
+    """Add verification flag to review model."""
 
     dependencies = [
         ('reviews', '0001_initial'),
