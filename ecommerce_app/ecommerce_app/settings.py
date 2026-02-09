9392470b08ba7038419b1faa17eb849bfb3a6b7b
WernerVal-8485 <werner.valentin@imqs.co.za>
2026-02-09 09:22:58 +0200

    mySQL changes

--- ecommerce_app/ecommerce_app/settings.py
+++ ecommerce_app/ecommerce_app/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'ecommerce_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME', 'ecommerce_db'),
-        'USER': os.getenv('DB_USER', 'root'),
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '3306'),
+        'NAME': 'ecommerce_db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
         'OPTIONS': {
             'charset': 'utf8mb4',
         },
